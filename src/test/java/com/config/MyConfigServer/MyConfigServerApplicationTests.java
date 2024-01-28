6113655b31a6de7e20a11a66d00bc84d8f1a6984
Asus <satty82@gmail.com>
2024-01-28 15:48:50 +0530

    Initial commit

---
+++ src/test/java/com/config/MyConfigServer/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.MyConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
